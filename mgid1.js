fd09cdd798a62c7f5fe42d5a44107722b201f240
irwanrohul <42856871+irwanrohul@users.noreply.github.com>
2018-09-06 14:26:59 +0700

    Update mgid1.js

--- mgid1.js
+++ mgid1.js
@@ -1,11 +1,5 @@
-&lt;div id=&quot;M356724ScriptRootC272154&quot;&gt;
-        &lt;div id=&quot;M356724PreloadC272154&quot;&gt;
-        Loading...    &lt;/div&gt;
-        &lt;script&gt;
-                (function(){
-            var D=new Date(),d=document,b=&#039;body&#039;,ce=&#039;createElement&#039;,ac=&#039;appendChild&#039;,st=&#039;style&#039;,ds=&#039;display&#039;,n=&#039;none&#039;,gi=&#039;getElementById&#039;,lp=d.location.protocol,wp=lp.indexOf(&#039;http&#039;)==0?lp:&#039;https:&#039;;
-            var i=d[ce](&#039;iframe&#039;);i[st][ds]=n;d[gi](&quot;M356724ScriptRootC272154&quot;)[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln(&quot;&lt;ht&quot;+&quot;ml&gt;&lt;bo&quot;+&quot;dy&gt;&lt;/bo&quot;+&quot;dy&gt;&lt;/ht&quot;+&quot;ml&gt;&quot;);iw.close();var c=iw[b];}
-            catch(e){var iw=d;var c=d[gi](&quot;M356724ScriptRootC272154&quot;);}var dv=iw[ce](&#039;div&#039;);dv.id=&quot;MG_ID&quot;;dv[st][ds]=n;dv.innerHTML=272154;c[ac](dv);
-            var s=iw[ce](&#039;script&#039;);s.async=&#039;async&#039;;s.defer=&#039;defer&#039;;s.charset=&#039;utf-8&#039;;s.src=wp+&quot;//jsc.mgid.com/l/i/lintasberita.net.272154.js?t=&quot;+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
-    &lt;/script&gt;
-    &lt;/div&gt;
+(function(){
+            var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
+            var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M356724ScriptRootC270442")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
+            catch(e){var iw=d;var c=d[gi]("M356724ScriptRootC270442");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=270442;c[ac](dv);
+            var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/l/i/lintasberita.net.270442.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
